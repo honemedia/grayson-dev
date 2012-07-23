71803f313a445a453649061ae08a11e962de4e41
Phil Huffstickler <phil@honemedia.com>
2012-07-23 12:18:01 -0500

    removed inline styles for contact us page

--- contact.php
+++ contact.php
@@ -6,30 +6,28 @@
 		<nav id='nav' role='navigation'></nav>
 	</header>
 	
-	
-	
 	<div id="contact-side-container">
-		<div class="sectionTitle">Contact Us</div>
+		<div class="section-title">Contact Us</div>
 		<div>Have a question about our company or our products? Please let us know... {ENTER MORE CONTENT HERE}</div>
 		
-		<div class="contact-title"><img style="margin-right:10px; float:left; padding:0 10px 50px 0" src="media/img/arrow.png">ADDRESS
-			<div style="color:#D4CBAC;">504 N Glenfield Road<br />New Albany, MS 38652</div>
+		<div class="contact-title"><img class="contact-title-arrow" src="media/img/arrow.png">ADDRESS
+			<div class="contact-description">504 N Glenfield Road<br />New Albany, MS 38652</div>
 		</div>
 		
-		<div class="contact-title""><img style="margin-right:10px; float:left; padding:0 10px 50px 0" src="media/img/arrow.png">PHONE
-			<div style="color:#D4CBAC;">(662)534-9800</div>
+		<div class="contact-title""><img class="contact-title-arrow" src="media/img/arrow.png">PHONE
+			<div class="contact-description">(662)534-9800</div>
 		</div>
 		
-		<div class="contact-title"><img style="margin-right:10px; float:left; padding:0 10px 50px 0" src="media/img/arrow.png">FAX
-			<div style="color:#D4CBAC;">(662)534-9805</div>
+		<div class="contact-title"><img class="contact-title-arrow" src="media/img/arrow.png">FAX
+			<div class="contact-description">(662)534-9805</div>
 		</div>
 		
-		<div class="contact-title"><img style="margin-right:10px; float:left; padding:0 10px 50px 0" src="media/img/arrow.png">EMAIL SALES
-			<div style="color:#D4CBAC;">sales@graysonfurniture.com</div>
+		<div class="contact-title"><img class="contact-title-arrow" src="media/img/arrow.png">EMAIL SALES
+			<div class="contact-description">sales@graysonfurniture.com</div>
 		</div>
 		
-		<div class="contact-title"><img style="margin-right:10px; float:left; padding:0 10px 50px 0" src="media/img/arrow.png">EMAIL SUPPORT
-			<div style="color:#D4CBAC;">support@graysonfurniture.com</div>
+		<div class="contact-title"><img class="contact-title-arrow" src="media/img/arrow.png">EMAIL SUPPORT
+			<div class="contact-description">support@graysonfurniture.com</div>
 		</div>
 		
 		
